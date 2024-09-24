2b01be3c65a86deef5217febf2e4a4bd37f3bd21
Cristopher <cristovao.santos@etec.sp.gov.br>
2024-09-23 21:43:22 -0300

    Aula php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>aula php</title>
+</head>
+<body>
+    <form action="script.php" method="post">
+        <label for="nome">Nome</label><br>
+        <input type="text" name="nome" id="nome"> <br><br>
+        <label for="nota1">Nota1</label><br>
+        <input type="number" name="nota1" id="nota1"><br><br>
+        <label for="nota2">Nota2</label><br>
+        <input type="number" name="nota2" id="nota2"><br><br>
+        <label for="nota3">Nota3</label><br>
+        <input type="number" name="nota3" id="nota3"><br><br>
+        <label for="nota4">Nota4</label><br>
+        <input type="number" name="nota4" id="nota4"><br><br>
+
+        <button type="submit">Enviar</button>
+
+    </form>
+</body>
+</html>
